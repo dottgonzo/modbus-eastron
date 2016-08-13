bc1e62f86e8216ef033d01ff38a6e17df1423a7f
Dario Caruso <darioyzf@gmail.com>
2016-08-14 00:23:25 +0200

    start to rewrite the code

---
+++ index.ts
@@ -0,0 +1,164 @@
+
+import * as async from "async";
+import * as Promise from "bluebird";
+import lsusbdev from "lsusbdev";
+
+const mrtu = require("modbus-serial");
+
+const Models: IModel[] = require('./models.json');
+let Model;
+
+
+const Client = new mrtu();
+
+interface IRegister {
+    label: string;
+    reg: number;
+    group?: string;
+}
+
+
+interface IModel {
+    label: string;
+    registers: IRegister[];
+}
+
+
+
+
+
+interface EastronDevice {
+    dev?: string;
+    hub?: string;
+    baud: number;
+    id: number;
+    uid?: string;
+    model: string;
+}
+
+
+export default (o: EastronDevice) => {
+    return new Promise((resolve, reject) => {
+
+        let answer:any={};
+
+        let registers: IRegister[];
+
+        function availablemodels(): string[] {
+            let models: string[] = [];
+
+            for (let i = 0; i < Models.length; i++) {
+
+                models.push(Models[i].label);
+
+            }
+            return models
+        }
+
+
+
+        function connectRTU() {
+
+            function start() {
+
+                Client.setID(o.id);
+                async.eachSeries(registers, (regi, cb) => {
+
+                    Client.readInputRegisters(regi.reg, 2, (err, data) => {
+console.log(data)
+                        if (err) {
+                            cb(err)
+                        } else {
+
+                            if (regi.group) {
+                                if (!answer[regi.group]) answer[regi.group] = {}
+                                answer[regi.group][regi.label] = data.buffer.readFloatBE()
+                            } else {
+                                answer[regi.label] = data.buffer.readFloatBE()
+                            }
+                            cb()
+                        }
+
+                    })
+                }, (err) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        const pkg = require("./package.json")
+                        answer.apiVersion = pkg.name + ' - ' + pkg.version;
+
+                        if (o.uid) {
+                            answer.uid = o.uid;
+                            answer._id = "consumption_" + o.uid;
+                        }
+
+                        answer.unixTimestamp = new Date().getTime();
+                        resolve(answer)
+                    }
+                })
+            }
+
+            Client.setTimeout(10000);
+
+            Client.connectRTUBuffered(o.dev, { baudrate: o.baud }, start);
+
+
+        }
+
+
+
+
+        if (!o) {
+            reject("No conf provided")
+        } else if (!o.dev && !o.hub) {
+            reject("No dev")
+        } else if (!o.model) {
+            reject("No model provided. Available models are: " + availablemodels())
+        } else {
+
+
+            for (let i = 0; i < Models.length; i++) {
+                if (Models[i].label === o.model) {
+                    registers = Models[i].registers;
+                }
+            }
+
+            console.log(registers)
+
+
+            if (registers && registers.length && registers[0] && (registers[0].reg || registers[0].reg === 0) && registers[0].label) {
+                if (o.dev) {
+
+                    connectRTU();
+
+                } else if (!o.dev && o.hub) {
+                    lsusbdev().then((data) => {
+
+                        for (let i = 0; i < data.length; i++) {
+                            if (data[i].hub === o.hub) {
+                                o.dev = data[i].dev;
+                            }
+                        }
+
+                        if (o.dev) {
+                            connectRTU();
+
+                        } else {
+                            reject("No dev for " + o.hub)
+
+                        }
+
+                    }).catch((err) => {
+                        reject(err)
+
+                    })
+
+
+                }
+
+            } else {
+                reject("No model founded. Available models are: " + availablemodels())
+            }
+        }
+    })
+}
---
+++ test/sdm120-ct-dev.ts
@@ -0,0 +1,70 @@
+import sdm120ct from "../index"
+
+import * as chai from "chai";
+
+const expect = chai.expect;
+
+interface Ipowa {
+    power: number;
+    import: number;
+    export: number;
+    total: number;
+}
+
+
+
+
+
+interface IEastron120CT {
+    _id?: string;
+    uid?: string;
+    voltage: number;
+    current: number;
+    power: number;
+    factor: number;
+    phaseAngle: number;
+    hz: number;
+    active: Ipowa;
+    reactive: Ipowa;
+    apiVersion: string;
+    unixTimestamp: number;
+}
+
+
+describe("main test", function () {
+    this.timeout(50000);
+
+    it("should return an object", function (done) {
+
+        sdm120ct({ baud: 2400, id: 1, dev: "/dev/ttyUSB0", model: 'SDM120CT' }).then((a: IEastron120CT) => {
+
+            expect(a).to.be.an('Object');
+            expect(a.active).to.be.an('Object');
+            expect(a.reactive).to.be.an('Object');
+            expect(a.voltage).to.be.a('number');
+            expect(a.current).to.be.a('number');
+            expect(a.power).to.be.a('number');
+            expect(a.factor).to.be.a('number');
+            expect(a.phaseAngle).to.be.a('number');
+            expect(a.hz).to.be.a('number');
+
+            expect(a.active.import).to.be.a('number');
+            expect(a.active.export).to.be.a('number');
+            expect(a.active.total).to.be.a('number');
+            expect(a.active.import).to.be.a('number');
+            expect(a.active.export).to.be.a('number');
+            expect(a.active.total).to.be.a('number');
+
+
+            expect(a.apiVersion).to.be.a('string');
+            expect(a.unixTimestamp).to.be.a('number');
+
+
+            done()
+        }).catch((err) => {
+            done(Error(err))
+        })
+
+    });
+
+});
