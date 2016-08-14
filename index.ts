b863e68388fe20bc8fbd628647389c96ce22a669
Dario Caruso <darioyzf@gmail.com>
2016-08-14 19:47:13 +0200

    can i close?

--- index.ts
+++ index.ts
@@ -81,6 +81,7 @@ export default (o: EastronDevice) => {
 
                     })
                 }, (err) => {
+                    Client.close();
                     if (err) {
                         reject(err)
                     } else {
